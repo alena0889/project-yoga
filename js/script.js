effaaa420c38d5179c7b562a90e857e06b6f51c1
alena0889 <al.voronova0889@gmail.com>
2019-12-16 15:16:08 +0400

    modal window

--- js/script.js
+++ js/script.js
@@ -84,4 +84,38 @@ function setClock(id, endtime) {
 }
 
 setClock('timer', deadline);
+
+// modal window
+
+let more = document.querySelector(".more"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close"),
+    descriptionBtns = document.querySelectorAll('.description-btn');
+
+
+    for (let i = 0; i < descriptionBtns.length; i++) {
+
+    descriptionBtns[i].addEventListener('click', function() {
+            overlay.style.display  = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        
+        })
+    }
+        
+    more.addEventListener('click', function() {
+        overlay.style.display  = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+
+    })
+
+    close.addEventListener('click', function() {
+       overlay.style.display = "none";
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    })
+
+ 
+
 });
